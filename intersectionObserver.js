5eb12416bb02098542e952b542c6c55752ae0228
adwoj92 <ad.woj92@gmail.com>
2022-06-18 08:36:08 +0100

    first commit

---
+++ intersectionObserver.js
@@ -0,0 +1,21 @@
+const image = document.querySelector('.imageOfMe');
+const bio = document.querySelector('.bio');
+
+const options = {
+    root: null,
+    threshold: 0,
+    rootMargin: "-200px"
+};
+
+const observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        }
+        entry.target.classList.toggle("visible");
+        observer.unobserve(entry.target);
+    });
+}, options);
+
+observer.observe(image);
+observer.observe(bio);
